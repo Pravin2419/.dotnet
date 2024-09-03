94189944f314e07bbdd41dfd4620de8dc3d80cd6
pravin patil <108794877+Pravin2419@users.noreply.github.com>
2024-09-03 11:23:20 +0530

    Add files via upload

---
+++ Imageupload_controller.cs
@@ -0,0 +1,33 @@
+﻿using image_p.DTO;
+using image_p.Models;
+using image_p.Repository.Interface;
+using image_p.Service.ImplementaionService;
+using image_p.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace image_p.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Image_controller : ControllerBase
+    {
+        private readonly Iimageservice iimageservice;
+
+        public Image_controller(Iimageservice iimageservice)
+        {
+            this.iimageservice = iimageservice;
+        }
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadImage([FromForm] ImageDto imageDto)
+        {
+            if (imageDto.ImageUrl == null || imageDto.ImageUrl.Length == 0)
+            {
+                return BadRequest("No file uploaded");
+            }
+
+            await iimageservice.UploadImageAsync(imageDto);
+            return Ok("Image uploaded successfully");
+        }
+    }
+}
